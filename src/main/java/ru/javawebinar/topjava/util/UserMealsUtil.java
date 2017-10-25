32cd5916ed7a5adc5d06d34c584a98988bab1859
igel911 <igel911@yandex.ru>
2017-10-25 21:23:08 +0300

    HW0 Ver.2 добавил метод на циклах с исправленной логикой

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -26,15 +26,29 @@ public class UserMealsUtil {
         getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
     }
 
+//    public static List<UserMealWithExceed>  getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+//        Map<UserMeal, Integer> map = mealList.stream()
+//                .collect(Collectors.toMap(
+//                        um -> um, UserMeal::getCalories, (k1, k2) -> k1 + k2,
+//                        () -> new TreeMap<>(Comparator.comparing(o -> o.getDateTime().toLocalDate()))
+//                ));
+//        return mealList.stream()
+//                .filter(um -> TimeUtil.isBetween(um.getDateTime().toLocalTime(), startTime, endTime))
+//                .map(um -> new UserMealWithExceed(um.getDateTime(), um.getDescription(), um.getCalories(), map.get(um) > caloriesPerDay))
+//                .collect(Collectors.toList());
+//    }
+
     public static List<UserMealWithExceed>  getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        Map<UserMeal, Integer> map = mealList.stream()
-                .collect(Collectors.toMap(
-                        um -> um, UserMeal::getCalories, (k1, k2) -> k1 + k2,
-                        () -> new TreeMap<>(Comparator.comparing(o -> o.getDateTime().toLocalDate()))
-                ));
-        return mealList.stream()
-                .filter(um -> TimeUtil.isBetween(um.getDateTime().toLocalTime(), startTime, endTime))
-                .map(um -> new UserMealWithExceed(um.getDateTime(), um.getDescription(), um.getCalories(), map.get(um) > caloriesPerDay))
-                .collect(Collectors.toList());
+        Map<UserMeal, Integer> map = new TreeMap<>(Comparator.comparing(o -> o.getDateTime().toLocalDate()));
+        for (UserMeal uM : mealList) {
+            map.merge(uM, uM.getCalories(), (int1, int2) -> int1 + int2);
+        }
+        List<UserMealWithExceed> list = new ArrayList<>(map.keySet().size());
+        for (UserMeal uM : mealList) {
+            if (TimeUtil.isBetween(uM.getDateTime().toLocalTime(), startTime, endTime)){
+                list.add(new UserMealWithExceed(uM.getDateTime(), uM.getDescription(), uM.getCalories(), map.get(uM) > caloriesPerDay));
+            }
+        }
+        return list;
     }
 }
