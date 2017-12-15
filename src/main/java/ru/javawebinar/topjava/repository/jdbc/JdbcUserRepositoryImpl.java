345f807d4feffc9a85a73a6049eabe91478d6167
igel911 <igel911@yandex.ru>
2017-12-15 22:34:59 +0200

    HW6 methods get, getByEmail and getAll (JdbcUserRepositoryImpl) work with ResultSetExtractor

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepositoryImpl.java
@@ -1,9 +1,11 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
@@ -14,6 +16,8 @@ import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
 import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -55,7 +59,6 @@ public class JdbcUserRepositoryImpl implements UserRepository {
             }
             jdbcTemplate.update("DELETE FROM user_roles WHERE user_id=?", user.getId());
         }
-        //user.getRoles().forEach(role -> jdbcTemplate.update("INSERT INTO user_roles VALUES (?,?)", user.getId(), role.name()));
         String[] queries = user.getRoles()
                 .stream()
                 .map(role -> String.format("INSERT INTO user_roles VALUES (%s,'%s')", user.getId(), role.name()))
@@ -72,41 +75,47 @@ public class JdbcUserRepositoryImpl implements UserRepository {
 
     @Override
     public User get(int id) {
-        List<User> users = jdbcTemplate.query("SELECT * FROM users WHERE id=?", ROW_MAPPER, id);
-        if (!users.isEmpty()){
-            User user = DataAccessUtils.singleResult(users);
-            user.setRoles(getRoles(user.getId()));
-            return user;
-        }
-        return null;
+        List<User> users = jdbcTemplate.query(
+                "SELECT * FROM users LEFT JOIN user_roles ON users.id = user_roles.user_id  WHERE id=?"
+                , getResultSetExtractor(), id);
+        return DataAccessUtils.singleResult(users);
     }
 
     @Override
     public User getByEmail(String email) {
-//        return jdbcTemplate.queryForObject("SELECT * FROM users WHERE email=?", ROW_MAPPER, email);
-        List<User> users = jdbcTemplate.query("SELECT * FROM users WHERE email=?", ROW_MAPPER, email);
-        if (!users.isEmpty()){
-            User user = DataAccessUtils.singleResult(users);
-            user.setRoles(getRoles(user.getId()));
-            return user;
-        }
-        return null;
+        List<User> users = jdbcTemplate.query(
+                "SELECT * FROM users LEFT JOIN user_roles ON users.id = user_roles.user_id WHERE email=?"
+                , getResultSetExtractor(), email);
+        return DataAccessUtils.singleResult(users);
     }
 
     @Override
     public List<User> getAll() {
-        List<User> users = jdbcTemplate.query("SELECT * FROM users ORDER BY name, email", ROW_MAPPER);
-        users.forEach(user -> user.setRoles(getRoles(user.getId())));
-        return users;
+        return jdbcTemplate.query("SELECT * FROM users LEFT JOIN user_roles " +
+                "ON users.id = user_roles.user_id ORDER BY name, email", getResultSetExtractor());
     }
 
-    private List<Role> getRoles(int userId) {
-        List<Role> roles = jdbcTemplate.queryForList(
-                "SELECT role FROM user_roles WHERE user_id=?"
-                , String.class, userId)
-                .stream()
-                .map(Role::valueOf)
-                .collect(Collectors.toList());
-        return Objects.nonNull(roles) ? roles : Collections.emptyList();
+    private ResultSetExtractor<List<User>> getResultSetExtractor() {
+        return rs -> {
+            List<User> users = new ArrayList<>();
+            while (rs.next()) {
+                User user = new User();
+                user.setId(rs.getInt("id"));
+                user.setName(rs.getString("name"));
+                user.setEmail(rs.getString("email"));
+                user.setPassword(rs.getString("password"));
+                user.setRegistered(rs.getDate("registered"));
+                user.setEnabled(rs.getBoolean("enabled"));
+                user.setCaloriesPerDay(rs.getInt("calories_per_day"));
+                String sRole = rs.getString("role");
+                if (users.contains(user)) {
+                    users.get(users.indexOf(user)).getRoles().add(Role.valueOf(sRole));
+                } else if (Objects.nonNull(sRole)){
+                    user.setRoles(EnumSet.of(Role.valueOf(sRole)));
+                    users.add(user);
+                }
+            }
+            return users;
+        };
     }
 }
